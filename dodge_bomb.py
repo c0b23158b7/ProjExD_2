bf1aef10e78397d5598890da63eef9986bcb8d21
c0b23158b7 <c0b23158b7@edu.teu.ac.jp>
2024-06-18 16:59:58 +0900

    1.飛ぶ方向に従ってこうかとん画像を切り替える

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,17 +3,15 @@ import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1600, 900
-DELTA = { # 移動量辞書
-    pg.K_UP : (0, -5),
-    pg.K_DOWN : (0, +5),
-    pg.K_LEFT : (-5, 0),
-    pg.K_RIGHT : (+5, 0)
+DELTA = {  # 移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
 }
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
@@ -29,43 +27,66 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
+    kk_flip_img = pg.transform.flip(kk_img, True, False)  # 反転画像を作成
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    
-    bb_img = pg.Surface((20, 20)) #1辺が28の空のSurfaceを作る
-    pg.draw.circle(bb_img, (255, 0, 0),(10, 10), 10) #空のSurfaceに赤い円を描く
-    bb_rct = bb_img.get_rect() # 爆弾Rect
+
+    bb_img = pg.Surface((20, 20))  # 1辺が20の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, +5 # 爆弾の横方向速度、縦方向速度
-    
+    vx, vy = +5, +5  # 爆弾の横方向速度、縦方向速度
+
     clock = pg.time.Clock()
     tmr = 0
+    kokaton = kk_img
+    
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
         if kk_rct.colliderect(bb_rct):  # 衝突判定
             return  # ゲームオーバー
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        
+
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-        kk_rct.move_ip(sum_mv)
         
+        if sum_mv==[-5,0]:
+            kokaton = kk_img
+        elif sum_mv==[-5,-5]:
+            kokaton = pg.transform.rotozoom(kk_img,-45,1)
+        elif sum_mv==[-5, +5]:
+            kokaton = pg.transform.rotozoom(kk_img,45,1)
+        elif sum_mv==[+5, 0]:
+            kokaton = kk_flip_img
+        elif sum_mv==[+5, -5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,45,1)
+        elif sum_mv==[0, -5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,90,1)
+        elif sum_mv==[+5, +5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,-45,1)
+        elif sum_mv==[0, +5]:
+            kokaton = pg.transform.rotozoom(kk_flip_img,-90,1)
+            
+
+        kk_rct.move_ip(sum_mv)
+
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
-        
+        screen.blit(kokaton, kk_rct)
+
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 横方向にはみ出たら
